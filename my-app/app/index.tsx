c22910f968ab966f35db8caa9976b2bbf1dc896f
Amina Fayaz <aminafayaz1077@gmail.com>
2025-05-23 11:12:51 +0530

    added text

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -2,14 +2,13 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      
+    <View>
+      <Text>LoRaWAN Kerala</Text>
     </View>
   );
 }
+// style={{
+//         flex: 1,
+//         justifyContent: "center",
+//         alignItems: "center",
+//       }}
