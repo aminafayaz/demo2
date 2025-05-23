ae029c2d76aaeb9811f9a9d380f95e053d5a42d4
Neil Joe Augustine <neil.joeaugustine@gmail.com>
2025-05-23 12:19:05 +0530

    my commit

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -5,12 +5,22 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function Index() {
   return (
+<<<<<<< Updated upstream
     <View style={styles.border}>
 
       <Text style = {styles.innertext}>LoRaWAN Kerala</Text>
       <View>
         <AntDesign name="infocirlce" size={24} color="red" />
         <MaterialIcons name="double-arrow" size={24} color="red" />
+=======
+    <View style={[styles.align,styles.border, styles.mar, styles.pad,styles.justify]}>
+      <View style={[styles.align]}>
+        <Text style = {styles.innertext}>LoRaWAN Kerala</Text>
+      </View>
+      <View style={{alignItems: 'flex-end', flexDirection: 'column'}}>
+        <Text style = {styles.innertext}>Ker</Text>
+
+>>>>>>> Stashed changes
       </View>
     </View>
   );
@@ -32,5 +42,20 @@ const styles = StyleSheet.create({
    border: {
     borderWidth: 2,
     borderColor: 'red',
+  },
+  pad: {
+    paddingTop: 10,
+    paddingLeft: 5,
+    paddingBottom: 10,
+  },
+  mar:{
+    margin: 10,
+  },
+  align:{
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+  justify:{
+    justifyContent: 'space-around',
   }
 })
