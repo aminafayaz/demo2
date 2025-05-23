d2fd4a43654c37638fa1889f6795ce9451649b02
Amina Fayaz <aminafayaz1077@gmail.com>
2025-05-23 14:22:10 +0530

    added new parameter page

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,19 +1,25 @@
-import {StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-import AntDesign from '@expo/vector-icons/AntDesign';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function Index() {
   return (
-    <View style={[styles.align,styles.border, styles.mar, styles.pad,styles.justify]}>
-
-      <Text style = {styles.innertext}>LoRaWAN Kerala</Text>
-      <View style = {styles.align}>
-   <AntDesign name="infocirlce" size={24} color="red" />
+    <View
+      style={[
+        styles.align,
+        styles.border,
+        styles.mar,
+        styles.pad,
+        styles.justify,
+      ]}
+    >
+      <Text style={styles.innertext}>LoRaWAN Kerala</Text>
+      <View style={styles.align}>
+        <AntDesign name="infocirlce" size={24} color="red" />
         <MaterialIcons name="double-arrow" size={24} color="red" />
       </View>
-      </View>
-    
+    </View>
   );
 }
 // style={{
@@ -22,31 +28,30 @@ export default function Index() {
 //         alignItems: "center",
 //       }}
 
-
 const styles = StyleSheet.create({
-  innertext :{
-    fontSize :25,
-    fontWeight :'bold',
-    color: 'red',
-    textAlign: 'center'
+  innertext: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "red",
+    textAlign: "center",
   },
-   border: {
+  border: {
     borderWidth: 2,
-    borderColor: 'red',
+    borderColor: "red",
   },
   pad: {
     paddingTop: 10,
     paddingLeft: 5,
     paddingBottom: 10,
   },
-  mar:{
+  mar: {
     margin: 10,
   },
-  align:{
-    flexDirection: 'row',
-    alignItems: 'flex-start',
+  align: {
+    flexDirection: "row",
+    alignItems: "flex-start",
   },
-  justify:{
-    justifyContent: 'space-around',
-  }
-})
+  justify: {
+    justifyContent: "space-around",
+  },
+});
